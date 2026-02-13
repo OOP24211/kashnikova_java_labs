328efbfac8e453c37adc88bd694974874a8e8c00
Kashnikova Evelina <36070988@mail.ru>
2026-02-13 19:32:55 +0700

    add lab1

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        if (args.length < 2) {
+            System.out.println("Usage: java Main <input_file> <output_file>");
+            return;
+        }
+
+        WordCounter counter = new WordCounter();
+
+        try (Scanner scanner = new Scanner(new File(args[0]), StandardCharsets.UTF_8.name())) {
+            while (scanner.hasNext()) {
+                String word = scanner.next();
+                counter.addRawWord(word);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Not open input");
+            return;
+        }
+
+        SortOutput.sort(counter.getWords(), args[1], counter.getTotalWords());
+    }
+}
