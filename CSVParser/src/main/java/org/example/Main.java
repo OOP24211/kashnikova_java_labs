305822c57682db38c12114b85568bbe1e2c74a40
Kashnikova Evelina <36070988@mail.ru>
2026-02-15 18:09:26 +0700

    Исправила обработку ошибки при вводе данных

--- CSVParser/src/main/java/org/example/Main.java
+++ CSVParser/src/main/java/org/example/Main.java
@@ -5,10 +5,9 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        if (args.length < 2) {
+        if (args.length != 2) {
             System.out.println("Usage: java Main <input_file> <output_file>");
             return;
-        }
 
         WordCounter counter = new WordCounter();
 
